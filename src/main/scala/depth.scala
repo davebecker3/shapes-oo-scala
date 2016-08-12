98174850f27f46c901769aace2235ea88cf39a90
davebecker3 <dave.becker@gmail.com>
2016-08-11 21:09:07 -0500

    Added depth and testDepth functions.

--- src/main/scala/depth.scala
+++ src/main/scala/depth.scala
@@ -2,17 +2,15 @@ package edu.luc.cs.laufer.cs372.shapes
 
 import edu.luc.cs.laufer.cs372.shapes
 
-// File copied from "boundingBox.scala".
+// File copied from "size.scala".
 // Function based on copy of function "size" in "orgchart.sc" in "misc-explorations-scala".
 
-object size {
+object depth {
   def apply(s: Shape): Int = s match {
     case Rectangle(_, _) => 1
     case Ellipse(_, _) => 1
-    case Location(_, _, shape @ _) => size(shape)
-    case Group(shapes @ _*) => shapes.map(size(_)).sum
+    case Location(_, _, shape @ _) => depth(shape) + 1
+    case Group(shapes @ _*) => shapes.map(depth(_)).max + 1
     case _ => {println("Function \"size\" received object of unknown type."); 0}
   }
 }
-
-
---
+++ src/test/scala/TestDepth.scala
@@ -0,0 +1,24 @@
+package edu.luc.cs.laufer.cs372.shapes
+
+import org.scalatest.FunSuite
+
+import TestFixtures._
+
+// Copied file from "TestSize.scala".
+
+class TestDepth extends FunSuite {
+
+  def testDepth(description: String, s: Shape, objective: Int) = {
+    test(description) {
+      val result = depth(s)
+      assert(objective === result)
+    }
+  }
+
+  testDepth("simple ellipse", simpleEllipse, 1)
+  testDepth("simple rectangle", simpleRectangle, 1)
+  testDepth("simple location", simpleLocation, 2)
+  testDepth("basic group", basicGroup, 2)
+  testDepth("simple group", simpleGroup, 3)
+  testDepth("complex group", complexGroup, 6)
+}
